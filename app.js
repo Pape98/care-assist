3fe5d06a72b94b6664bff4c5a9acb4cb38f726a0
Pape Sow Traore <papisline98@hotmail.fr>
2020-03-01 12:14:21 -0600

    fixing layout

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-
+// routers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var patientsRouter = require('./routes/patients');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
