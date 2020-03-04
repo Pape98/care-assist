ea7d11cbe92f7b163635022d10353700b888f1ad
mccullohb <mccullohb@tamu.edu>
2020-03-04 01:40:52 -0600

    added password hash and compare methods

--- db/User.js
+++ db/User.js
@@ -11,6 +11,7 @@
 
 // TESTING: Using mongoose db platform
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var schema = mongoose.Schema;
 // Define user attributes
 var userSchema = new schema({
@@ -27,6 +28,21 @@ var userSchema = new schema({
     //     required:false,
     // }
 })
+
+/*  Password Hash Method
+    - Hash password using bcrypt encryption scheme
+*/
+userSchema.methods.hashPassword = function (password) {
+    // Hash password using auto-generated salt (10 rounds)
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
+/*  Password Compare Method
+    -Accepts presumed password and accompanying hash to check for equivalence
+*/
+userSchema.methods.comparePassword = function (password, hash) {
+    return bcrypt.compareSync(password,hash);
+}
 
 // module name, schema, and collection name
 module.exports = mongoose.model('users', userSchema,'users');
