40d035643e9d92eb8f13b4c0910baaa9765dcb63
mccullohb <mccullohb@tamu.edu>
2020-03-17 17:59:39 -0400

    modified app.js & views; created new user model

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-// -----------------------------------------------
-/* Manual Additions:
-   Use express session for session storing, 
-   Passport.js for authentication,
-   Redis for db
-*/
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-var session = require('express-session');
-var passport = require('passport');
-mongoose.connect('mongodb://localhost:27017/login')
-// var RedisStore = require('connect-redis')(session)
-// -----------------------------------------------
+// var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+// var session = require('express-session');
+// var passport = require('passport');
 
-// routers
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true})
+  .then(() => console.log('MongoDB successfully connected...'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
 var indexRouter = require('./routes/index');
-var patientsRouter = require('./routes/patients');
-var app = express();
+var usersRouter = require('./routes/users');
+// var authRouter = require('./routes/auth')(passport);
+// Pair Routes with subdirectories
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// app.use('/auth', authRouter)
+
+
+// Define server port
+const PORT = process.env.PORT || 5000;
+// USING `
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,27 +57,17 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-// -----------------------------------------------
-/* Manual Additions:
-  Session configuration
-  Use mongo temporarily 
-  Use redis for DB calls
-*/
-app.use(session({
-  // store: new RedisStore({
-  //   url: config.RedisStore.url
-  // }),
-  // secret: config.RedisStore.secret,
-  secret: 'sessionSecret',
-  resave:false,
-  saveUninitialized:false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-// -----------------------------------------------
 
-app.use('/', indexRouter);
-app.use('/patients', patientsRouter);
+
+// app.use(session({
+//   secret: 'sessionSecret',
+//   resave:false,
+//   saveUninitialized:false
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ models/User.js
@@ -0,0 +1,57 @@
+// Database schema for users
+
+// Encryption
+//const bcrypt = require('bcrypt-nodejs');
+
+/* 
+    Redis modifications
+*/
+// var redis = require('redis');
+// var schema = redis.Schema;
+// var userSchema = new schema({
+
+// TESTING: Using mongoose db platform
+const mongoose = require('mongoose');
+
+// Define user attributes
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    admin:{
+        type:Boolean,
+        default: false,
+    }
+});
+
+// Export module name and schema
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+// /*  Password Hash Method
+//     - Hash password using bcrypt encryption scheme
+// */
+// userSchema.methods.hashPassword = function (password) {
+//     // Hash password using auto-generated salt (10 rounds)
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+// }
+
+// /*  Password Compare Method
+//     -Accepts presumed password and accompanying hash to check for equivalence
+// */
+// userSchema.methods.comparePassword = function (password, hash) {
+//     return bcrypt.compareSync(password,hash);
+// }
