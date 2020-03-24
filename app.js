86ffe8bec97ffa707506adf7d9101da0b004cbed
Pape Sow Traore <papesowtraore@Papes-MacBook-Pro-2.local>
2020-03-24 16:04:17 -0500

    login step 1

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
 
-// routers
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// MongoDB connection 
+// TODO: Replace with Redis connection
+mongoose.connect(db, { useNewUrlParser: true})
+  .then(() => console.log('MongoDB successfully connected...'))
+  .catch(err => console.log(err));
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }))
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
 var indexRouter = require('./routes/index');
-var patientsRouter = require('./routes/patients');
+var usersRouter = require('./routes/users');
+// Pair Routes with subdirectories
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-var app = express();
+
+// Define server port
+const PORT = process.env.PORT || 5000;
+// USING `
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -28,9 +69,6 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
