305eac737b657f12eb2e7092465f9fa063cd2290
Pape Sow Traore <papesowtraore@Papes-MacBook-Pro-2.local>
2020-03-24 16:29:25 -0500

    final2

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+/*
+    URL with login credentials needed to connect to db
+    TODO: Redis swap
+*/
+
+module.exports = {
+    MongoURI: 'mongodb+srv://Dev123:Dev123@mongotestcluster-fldvt.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ models/User.js
@@ -0,0 +1,39 @@
+/*
+    DATABASE SCHEMA for Users (Caretakers)
+*/
+
+// TODO: Redis swap
+// TESTING: Using mongoose db platform
+const mongoose = require('mongoose');
+
+// TODO: Redis swap
+// Define user attributes
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    admin:{
+        type: Boolean,
+        default: false,
+    }
+});
+
+// TODO: Redis swap
+// Export module name and schema
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+
