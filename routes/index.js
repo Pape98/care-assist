15f22b4c148c448539576e230e317b5ebe2a7eed
traorefly <traorefly@tamu.edu>
2020-04-05 06:41:24 -0500

    ss

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,10 @@ router.get('/login',function(req,res,next){
   res.render('pages/landing/login');
 });
 
+// /* GET login page */
+// router.get('/loader',function(req,res,next){
+//   res.render('pages/landing/loader');
+// });
+
 
 module.exports = router;
