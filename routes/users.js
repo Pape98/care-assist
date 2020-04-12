332751acc967a9c097e3055184d7186712ae0113
mccullohb <mccullohb@tamu.edu>
2020-04-11 21:20:43 -0400

    Change password route addition

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,13 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 var moment = require('moment');
+const { ensureAuthenticated } = require('../config/auth');
 
 // User model
 const User = require('../models/User');
 
-// GET Register Page
-router.get('/register', (req, res) => res.render('register'));
+// // GET Register Page
+// router.get('/register', (req, res) => res.render('register'));
 
 // GET home page 
 router.get('/home', ensureAuthenticated, function (req, res, next) {
@@ -33,10 +34,14 @@ router.get('/reminders', function (req, res, next) {
     res.render('pages/users/reminder');
 });
 
-// Register Handl -- Submit information through POST
+// Register Handle -- Submit information through POST
 
 router.post('/register', (req, res) => {
     // Information access
+    if(req.user.admin != true) {
+        console.log("Not admin");
+        return;
+    }
     const {
         first_name,
         last_name,
@@ -87,6 +92,49 @@ router.post('/login', (req, res, next) => {
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/logout/loader');
+});
+
+
+// Change Password
+router.post('/changePassword', ensureAuthenticated, (req, res) => {
+    // Get params
+    const {
+        current_password,
+        new_password,
+        confirm_new_password
+    } = req.body;
+    // Compare new passwords 
+    if (new_password == confirm_new_password) {
+        // Hash current password and compared to stored, hashed user password
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(current_password, salt, (err, hash) => {
+                if (err) throw err;
+                // Compare current hash with stored user hash
+                console.log(req.user.email);
+                console.log(req.user.password);
+                console.log(hash);
+                console.log(current_password);
+                if (req.user.password == hash) {
+                    // Modify password if user successfully authenticates
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(new_password, salt, (err, hash2) => {
+                            if (err) throw err; 
+                            // Reassign user password
+                            console.log("Password successfully changed");
+                            req.user.password = hash2;
+                        });
+                    });
+                }
+                else {
+                    console.log("Password incorrect");
+                }
+            });
+        });
+    }
+    else {
+        console.log("Passwords don't match");
+    } 
+    return res.redirect('/users/settings');
 });
 
 
