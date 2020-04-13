94a05af080c0b5a4096c8baea3a666c6d1eeb14f
mccullohb <mccullohb@tamu.edu>
2020-04-13 15:29:27 -0400

    Update info functionality

--- routes/users.js
+++ routes/users.js
@@ -12,30 +12,26 @@ const { ensureAuthenticated } = require('../config/auth');
 // User model
 const User = require('../models/User');
 
-// // GET Register Page
-// router.get('/register', (req, res) => res.render('register'));
-
-// GET home page 
+// GET home page
 router.get('/home', ensureAuthenticated, function (req, res, next) {
     var date = moment().format('MMMM Do YYYY');
     res.render('pages/users/home', {
         date: date
     });
 });
-// GET settings page. 
+// GET settings page
 router.get('/settings', function (req, res, next) {
     res.render('pages/users/profile', {
         isAdmin: req.user.admin
     })
 });
 
-// GET reminder page. 
+// GET reminder page
 router.get('/reminders', function (req, res, next) {
     res.render('pages/users/reminder');
 });
 
-// Register Handle -- Submit information through POST
-
+// Register Handle
 router.post('/register', (req, res) => {
     // Information access
     if(req.user.admin != true) {
@@ -76,7 +72,7 @@ router.post('/register', (req, res) => {
 });
 
 
-// Login Handle -- Submit information through POST
+// Login Handle
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
         // Redirects on both success and fail
@@ -87,18 +83,17 @@ router.post('/login', (req, res, next) => {
 });
 
 
-// Logout Handle -- Submit through GET
-
+// Logout Handle
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/logout/loader');
 });
 
 
-// Change Password
+// Update Password Handle
 router.post('/changePassword', ensureAuthenticated, (req, res) => {
     // Get params
-    var {
+    const {
         current_password,
         new_password,
         confirm_new_password
@@ -108,7 +103,7 @@ router.post('/changePassword', ensureAuthenticated, (req, res) => {
         // Verify user
         bcrypt.compare(current_password, req.user.password, function(err, isMatch) {
             if (isMatch) {
-                // Modify password if user successfully authenticates
+                // Hash password and update
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.genSalt(10, (err, salt) => {
                         bcrypt.hash(new_password, salt, (err, hash) => {
@@ -135,6 +130,49 @@ router.post('/changePassword', ensureAuthenticated, (req, res) => {
     else {
         console.log("Passwords don't match");
     } 
+    return res.redirect('/users/settings');
+});
+
+
+// Reset Password Handle
+
+
+// Get reset password page
+
+
+// Change Information Handle
+router.post('/changeInfo', ensureAuthenticated, (req, res) => {
+    // Get params
+    var {
+        first_name,
+        last_name,
+        new_email
+    } = req.body;
+    // Update email
+    if ((new_email != req.user.email) && (new_email.length > 0)) {
+        console.log(new_email);
+        req.flash('success', 'Confirmation email sent to: ', new_email);
+    }
+    // Ensure non-empty fields
+    if (first_name.length == 0) {
+        first_name = req.user.first_name;
+    }
+    if (last_name.length == 0) {
+        last_name = req.user.last_name;
+    }
+    var info = {
+        "first_name": first_name,
+        "last_name": last_name
+    }
+    // Update name
+    User.findOneAndUpdate(
+        {"email": req.user.email}, 
+        {$set: info},
+        function(err) {
+            if (err) console.log(err);
+            req.flash('success', 'Information successfully updated.')
+        }
+    );
     return res.redirect('/users/settings');
 });
 
